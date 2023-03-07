6226e7762b3b196b114e41a8e9e6b37922a59409
EC2 Default User <126648712+taka0705@users.noreply.github.com>
2023-03-07 08:50:46 +0000

    [add]フラッシュメッセージまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,23 +4,39 @@ class BooksController < ApplicationController
     @books=Book.all
     @book=Book.new
   end
-  
+
   def create
    book=Book.new(book_params)
    book.save
-   redirect_to'/'
- end
+     flash[:notice]="Book was successfully created."
+     redirect_to book_path(book.id)
+  end
+
 
   def show
       @book=Book.find(params[:id])
   end
 
   def edit
-      @list=List.find(params[:id])
+      @book=Book.find(params[:id])
   end
-  
+
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice2]="Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    flash[:notice3]="Book was successfully destroyed."
+    redirect_to'/books'
+  end
+
   private
   def book_params
   params.require(:book).permit(:title,:body)
-end
+  end
 end
